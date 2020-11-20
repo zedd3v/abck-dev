00a08619c51f1ae249258a502ccf6f5a41312edf
ZedDev <43217974+zedd3v@users.noreply.github.com>
2020-11-20 09:14:34 +0100

    update

--- src/pages/Package.tsx
+++ src/pages/Package.tsx
@@ -6,41 +6,28 @@ import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 export default (): JSX.Element => {
   const code = `
-const Abck = require('abck').default; // import Abck from 'abck';
+import Abck from 'abck'; // const Abck = require('abck').default;
 
-Abck.validateCookie(['=='], "C0E79E904291F0101B699F08B90AE09C~-1~YAAQvnEGF1n4o7ByAQAASRpJwAQ+zePjLXV6UyhVffBSzV8Rjd2nU3orgVbMnYvAao6Xgw8Feycm9b55c0HQgOWFePtq2y1Cc/f+FhPcJQYsSgeFZz1Rs/pBhI6Db/aCcjJd9WFJxZ5G+LOsuFezyoJDmY2FK4BAfTYKQ1oPqKN36Rjmqm6b+KMn8wq2/4rukChJhuFD4b/UDEcqCAdMguRAlosZrDg5y1GTsiMnjOPzxSJLoqsPT9tVf2tjVJw94fLQcyNrYYSPDvliWmI4WDzg6zRpkjBjfmsiXnwtApPKDSoahrM3ykGSOdHAwJGuInCZcrDayC7VwXlPPYYAzO9nVP95qnaVAA==~-1~-1~-1");
-// => true
-
-const d3 = Abck.d3();
-
-Abck.gd(Date.now(), d3, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36", {
-  availWidth: 1680,
-  availHeight: 972,
-  width: 1680,
-  height: 1050,
-  innerWidth: 1680,
-  innerHeight: 939,
-  outerWidth: 1680,
-});
-// => Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36,uaend,12147,0030107,en-US,Gecko,3,0,0,0,391828,9560794,1680,972,1680,1050,1680,939,1680,,cpen:0,i1:0,dm:0,cwen:0,non:1,opc:0,fc:0,sc:0,wrc:1,isc:0,vib:1,bat:1,x11:0,x12:1,8276,0.744979299372,796244780392,loc:
-
-Abck.o9(d3);
-// => 914527596
-
-Abck.sed();
-// => "0,0,0,0,1,0,0"
-
-Abck.ab("test");
-// => 448
-
-Abck.np();
-// => "10321144241322243122"
-
-Abck.getmr(); //Needs to be executed inside an electron window
-// => "58,60,289,59,113,56,26,21,25,9,9,9,20,588,"
-
-Abck.od("0a46G5m17Vrp4o4c", "afSbep8yjnZUjq3aL010jO15Sawj2VZfdYK8uY90uxq");
-// => "7a74G7m23Vrp0o5c"
+function get_cf_date(): number;
+function uar(userAgent: string): string;
+function sed(): string;
+function x2(): number;
+function ab(t: string): number;
+function ff(a: number): string;
+function to(): { d3: number; o9: number };
+function jrs(a: number): number[];
+function isIgn(fidcnt: number, activeElement: Element, keyCode: number): number;
+function getFloatVal(a: number): string;
+function x1(): string;
+function od(x: string, y: string): string;
+function encode(a: string): string;
+function get_mn_params_from_abck(abck: string): (string | number)[][];
+function mn_get_current_challenges(abck: string): string[];
+function mn_s(a: string): number[];
+function bdm(a: number[], t: number): nmuber;
+function ats(a: number[]): string;
+function validateCookie(cookieChecks: string[], abck: string): boolean;
+function getmr(): string; // Only works correctly in browser
   `;
   return (
     <Row className="align-items-center justify-content-center p-3">
@@ -52,6 +39,9 @@ Abck.od("0a46G5m17Vrp4o4c", "afSbep8yjnZUjq3aL010jO15Sawj2VZfdYK8uY90uxq");
           <h3 className="mb-0">Installation</h3>
           <hr />
           <SyntaxHighlighter className="w-100" language="bash" style={atomDark}>
+            pnpm add abck
+          </SyntaxHighlighter>
+          <SyntaxHighlighter className="w-100 mb-5" language="bash" style={atomDark}>
             npm install abck
           </SyntaxHighlighter>
           <SyntaxHighlighter className="w-100 mb-5" language="bash" style={atomDark}>
