e0781fc506c9acee145b9f0c2562ea2fa5e4e684
qugust <43217974+zedd3v@users.noreply.github.com>
2022-06-16 05:01:58 +0200

    fix decoding

--- src/functions/parse/new/index.ts
+++ src/functions/parse/new/index.ts
@@ -1,4 +1,6 @@
-/* eslint-disable no-bitwise */
+import { getSeperator, isJSON, isBool, RawSensorJson } from './utils';
+import { secondDec, firstDec } from './dec';
+
 interface Divider {
   divider: string;
   endDivider?: string;
@@ -26,7 +28,7 @@ const dividers: Divider[] = [
   { divider: '-103', name: 'bmak.vcact' },
   { divider: '-106', name: 'bmak.aj_type + bmak.aj_indx' },
   { divider: '-115', name: 'bunch of stuff' },
-  { divider: '-112', name: `document.URL` },
+  { divider: '-112', name: 'document.URL' },
   { divider: '-119', name: 'bmak.mr' },
   { divider: '-122', name: 'sed' },
   { divider: '-123', name: 'mn_r part 1' },
@@ -108,77 +110,6 @@ function recursiveSplitByDivider(
   return finalParsedSensor;
 }
 
-const decodeArr = JSON.parse(
-  '[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,0,1,-1,2,3,4,5,-1,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,-1,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91]'
-);
-
-const decodeChar = (char: string, shiftedComp: number) => {
-  let pos = ' !#$%&()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~'.indexOf(
-    char
-  );
-
-  if (pos < 92) pos += 92;
-  pos -= shiftedComp % 92;
-  if (pos > 92) pos -= 92;
-
-  return String.fromCharCode(decodeArr.indexOf(pos));
-};
-
-const firstDec = (sensor: string, bmSzFirstComp: number): string => {
-  const dec = [];
-
-  let bmSzFC = bmSzFirstComp;
-
-  for (let i = 0; i < sensor.length; i += 1) {
-    const shifted = (bmSzFC >> 8) & 65535;
-    bmSzFC *= 65793;
-    bmSzFC &= 4294967295;
-    bmSzFC += 4282663;
-    bmSzFC &= 8388607;
-
-    const charCode = sensor.charCodeAt(i);
-    if (charCode < 32 || charCode === 39 || charCode === 34 || charCode === 92) {
-      dec[i] = sensor[i];
-    } else {
-      dec[i] = decodeChar(sensor[i], shifted);
-    }
-  }
-
-  return dec.join('');
-};
-
-const secondDec = (payload: string, bmSzSecondComp: number): string => {
-  const pcs = payload.split(',');
-  const mixingIndexes = [];
-  let bmSzSC = bmSzSecondComp;
-
-  for (let i = 0; i < pcs.length; i += 1) {
-    const x = ((bmSzSC >> 8) & 65535) % pcs.length;
-    bmSzSC *= 65793;
-    bmSzSC &= 4294967295;
-    bmSzSC += 4282663;
-    bmSzSC &= 8388607;
-
-    const y = ((bmSzSC >> 8) & 65535) % pcs.length;
-    bmSzSC *= 65793;
-    bmSzSC &= 4294967295;
-    bmSzSC += 4282663;
-    bmSzSC &= 8388607;
-
-    mixingIndexes.push([x, y]);
-  }
-
-  for (let i = mixingIndexes.length - 1; i >= 0; i -= 1) {
-    const [x, y] = mixingIndexes[i];
-
-    const pc = pcs[x];
-    pcs[x] = pcs[y];
-    pcs[y] = pc;
-  }
-
-  return pcs.join(',');
-};
-
 const parseBmSzComps = (sensor: string): number[] => {
   return sensor
     .slice(2)
@@ -187,17 +118,18 @@ const parseBmSzComps = (sensor: string): number[] => {
     .map((n) => Number(n));
 };
 
-const getSeperator = (cleanSensor: string): string => {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore: Object is possibly 'null'.
-  return /2([a-zA-Z0-9!@#%&\-_=;:<>,~]+)2\1(?:7a7)/i.exec(cleanSensor)[1];
-};
-
 export function ParseNewSensor(sensor: string, detailed: boolean): ParsedSensor | null {
   try {
-    const bmSzComps = parseBmSzComps(sensor);
+    let rawSensor = sensor.toString();
 
-    const dirty = sensor.split(';').slice(4).join(';');
+    const jsonCheck = isJSON(sensor);
+    if (!isBool(jsonCheck) && 'sensor_data' in (jsonCheck as RawSensorJson)) {
+      rawSensor = (jsonCheck as RawSensorJson).sensor_data;
+    }
+
+    const bmSzComps = parseBmSzComps(rawSensor);
+
+    const dirty = rawSensor.split(';').slice(4).join(';');
 
     const halfClean = firstDec(dirty, bmSzComps[0]);
 
